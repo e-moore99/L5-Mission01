eb80200ea907251a68a543bcee3665107b2b34d6
Evan Moore <evanm@missionreadyhq.com>
2024-11-19 09:55:19 +1300

    got api endpoint working, and it gives responses that are accurate and appropriate. just one small bug + styling to work on

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // this receives the data from the response
   const [carData, setCarData] = useState();
+  // this displays the image on the page
   const [image, setImage] = useState();
-
+//using this third useState to append to the form data, to use in the post request
+  const [imageFile, setImageFile] = useState(null);
+  // this is used to store the result text
+  const [result, setResult] = useState();
   
   const handleImgSubmit = (event) => {
-    setImage(URL.createObjectURL(event.target.files[0]));
+    const file = event.target.files[0]
+    if (file){
+    setImage(URL.createObjectURL(file));
+    setImageFile(file);
     // setImage(event.target.value);
+  } else {
+    console.log("no file uploaded")
+  }
   };
 
 
@@ -17,32 +28,69 @@ function App() {
     event.preventDefault();
     console.log("click!");
     try {
-      const fetchOptions = {
-        method: "POST",
-        headers: {
-          "Prediction-Key":
-            "3IttSAg3iBMHlTPRFHcMkXJ3ziDehGJGc8waS1jbdVJ2n4lLTsn5JQQJ99AKACYeBjFXJ3w3AAAIACOGVygv",
-          "Content-Type": "multipart/form-data",
-        },
-        body: JSON.stringify({
-          image: image,
-        }),
-        // URL: {image},
-      };
+      const formData = new FormData();
+      formData.append("image", imageFile);
       const response = await fetch(
         `https://mission01-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/87bd17aa-27c9-4b89-95c4-7f6120d0411c/detect/iterations/Iteration3/image`,
-        fetchOptions
+        {
+          method: "POST",
+          headers: {
+            "Prediction-Key":
+              "3IttSAg3iBMHlTPRFHcMkXJ3ziDehGJGc8waS1jbdVJ2n4lLTsn5JQQJ99AKACYeBjFXJ3w3AAAIACOGVygv",
+            // "Content-Type": "application/octet-stream",
+          },
+          body: formData,
+        }
       );
       const resDescr = await response.json();
-      console.log(resDescr.data);
-      setCarData(resDescr.data);
-      console.log(carData);
+      if (resDescr){
+        setCarData(resDescr)
+        console.log(resDescr)
+      }
+
+      if (carData.predictions && carData.predictions.length > 0) {
+        // Find the prediction with the highest probability
+        const bestPrediction = carData.predictions.reduce((max, prediction) => 
+          prediction.probability > max.probability ? prediction : max
+        );
+
+        // Output the highest probability and associated label
+        console.log(`Highest probability: ${bestPrediction.probability} for label: ${bestPrediction.tagName}`);
+        
+let results;
+switch (bestPrediction.tagName) {
+  case "Motorbike": //if (tagName === "Motorbike")
+  results = `We are ${(bestPrediction.probability*100).toFixed(1)}% sure this is your ${bestPrediction.tagName}. The annual insurance cost will be $400` 
+    break;
+  case "Sedan": 
+  results = `We are ${(bestPrediction.probability*100).toFixed(1)}% sure this is your ${bestPrediction.tagName}. The annual insurance cost will be $500`
+    break;
+  case "SUV": 
+  results = `We are ${(bestPrediction.probability*100).toFixed(1)}% sure this is your ${bestPrediction.tagName}. The annual insurance cost will be $600`
+    break;
+  case "Van": //if (fruit === "pear")
+  results = `We are ${(bestPrediction.probability*100).toFixed(1)}% sure this is your ${bestPrediction.tagName}. The annual insurance cost will be $700`
+    break;
+  default:
+    console.log("Vehicle not found :(");
+}
+
+setResult(results)
+
+      } else {
+        console.log('No predictions found.');
+      }
+
     } catch (err) {
       console.log(err);
     }
   };
 
-  console.log({ image });
+  // let carType = result.tagName;
+  // console.log(result)
+
+
+
   return (
     <>
       <h1>Turners Cars Insurance</h1>
@@ -57,6 +105,7 @@ function App() {
             type="file"
             id="imgFile"
             name="image"
+            accept="/image*" //must ensure the file being uploaded is an img
             onChange={handleImgSubmit}
           />
           <input type="submit" onClick={handleSubmit} />
@@ -64,10 +113,7 @@ function App() {
       </div>
       <div className="imgBox" id="imgBox">
        {image && <img src={image} alt="Your car" />}
-        <p>
-          This is your (car type) and the insurance premium will be ($xyz) per
-          annum
-        </p>
+        {carData && <p>{result}</p>}
       </div>
     </>
   );
