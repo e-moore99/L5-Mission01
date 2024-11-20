ebd8b39eaef644855c3cff239b97fa2124ddaedd
Evan Moore <evanm@missionreadyhq.com>
2024-11-21 11:34:49 +1300

    tidying up the code and relevant comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // this receives the data from the response
-  // const [carData, setCarData] = useState();
   // this displays the image on the page
   const [image, setImage] = useState();
   //using this third useState to append to the form data, to use in the post request
@@ -16,7 +14,6 @@ function App() {
     if (file) {
       setImage(URL.createObjectURL(file));
       setImageFile(file);
-      // setImage(event.target.value);
     } else {
       console.log("no file uploaded");
     }
@@ -26,7 +23,9 @@ function App() {
     event.preventDefault();
     console.log("click!");
     try {
+      // creating new form data to send in the post request
       const formData = new FormData();
+      // attaching the image file to the form data
       formData.append("image", imageFile);
       const response = await fetch(
         `https://mission01-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/87bd17aa-27c9-4b89-95c4-7f6120d0411c/detect/iterations/Iteration3/image`,
@@ -35,16 +34,11 @@ function App() {
           headers: {
             "Prediction-Key":
               "3IttSAg3iBMHlTPRFHcMkXJ3ziDehGJGc8waS1jbdVJ2n4lLTsn5JQQJ99AKACYeBjFXJ3w3AAAIACOGVygv",
-            // "Content-Type": "application/octet-stream",
           },
           body: formData,
         }
       );
       const resData = await response.json();
-      // if (resDescr) {
-      //   setCarData(resDescr);
-      //   console.log(resDescr);
-      // }
 
       //here, I am immediately taking the response data from the api and based on the response setting the result message
       if (resData && resData.predictions.length > 0) {
@@ -53,7 +47,7 @@ function App() {
           prediction.probability > max.probability ? prediction : max
         );
 
-        // Output the highest probability and associated label
+        // Output the highest probability and associated tagName (vehicle type label)
         console.log(
           `Highest probability: ${bestPrediction.probability} for label: ${bestPrediction.tagName}`
         );
@@ -62,26 +56,26 @@ function App() {
         switch (bestPrediction.tagName) {
           case "Motorbike": //if (tagName === "Motorbike")
             results = `We are ${(bestPrediction.probability * 100).toFixed(
-              1
+              2
             )}% sure this is your ${
               bestPrediction.tagName
             }. The annual insurance cost will be $400`;
             break;
           case "Sedan":
             results = `We are ${(bestPrediction.probability * 100).toFixed(
-              1
+              2
             )}% sure this is your ${
               bestPrediction.tagName
             }. The annual insurance cost will be $500`;
             break;
           case "SUV":
             results = `We are ${(bestPrediction.probability * 100).toFixed(
-              1
+              2
             )}% sure this is your ${
               bestPrediction.tagName
             }. The annual insurance cost will be $600`;
             break;
-          case "Van": //if (fruit === "pear")
+          case "Van":
             results = `We are ${(bestPrediction.probability * 100).toFixed(
               2
             )}% sure this is your ${
@@ -101,21 +95,18 @@ function App() {
     }
   };
 
-  // let carType = result.tagName;
-  // console.log(result)
-
   return (
     <>
-    <header id="header">
-
-      <h1>Turners Car Insurance</h1>
-    </header>
+      <header id="header">
+        <h1>Turners Car Insurance</h1>
+      </header>
       <div id="bodyBox">
-      <p>
-        Here at Turners, we believe in offering you great value. <br />
-        To get a quick quote for insurance coverage for your vehicle, upload a
-        picture of it below and we'll handle the rest!
-      </p><br />
+        <p>
+          Here at Turners, we believe in offering you great value. <br />
+          To get a quick quote for insurance coverage for your vehicle, upload a
+          picture of it below and we'll handle the rest!
+        </p>
+        <br />
         <form>
           <input
             type="file"
@@ -124,12 +115,12 @@ function App() {
             accept="/image*" //must ensure the file being uploaded is an img
             onChange={handleImgSubmit}
           />
-          <input type="submit" onClick={handleSubmit} className="button"/>
+          <input type="submit" onClick={handleSubmit} className="button" />
         </form>
-      <div className="imgBox" id="imgBox">
-        {image && <img src={image} alt="Your car" id="carImg"/>}
-        <p>{result}</p>
-      </div>
+        <div className="imgBox" id="imgBox">
+          {image && <img src={image} alt="Your car" id="carImg" />}
+          <p>{result}</p>
+        </div>
       </div>
     </>
   );
